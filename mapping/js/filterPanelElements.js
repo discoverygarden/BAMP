d15bb5541207e416e7a654c14cfc96315b5be202
TJ Lewis <fuel37@hotmail.com>
2012-04-10 11:07:56 -0300

    Initial Commit

---
+++ mapping/js/filterPanelElements.js
@@ -0,0 +1,67 @@
+Ext.onReady(function(){
+  var comboJoin = {
+    xtype: 'combo',
+    forceSelection: true,
+    store: Ext.create('Ext.data.ArrayStore', {
+      fields: [ 'join' ],
+      data: [['AND'],['OR']]
+    }),//End create store
+    displayField: 'join',
+    fieldLabel: 'Join Type',
+    queryMode: 'local',
+    selectOnTab: false,
+    name: 'join',
+  };
+
+  var comboField = {
+    xtype: 'combo',
+    store: 'filterFields',
+    displayField: 'field',
+    valueField: 'fieldId',
+    width: 400,
+    fieldLabel: 'Filter Column',
+    forceSelection: true,
+    autoSelect: false,
+    selectOnTab: false,
+    name: 'column',
+  };
+
+  var comboOperator = {
+    xtype: 'combo',
+    forceSelection: true,
+    store: Ext.create('Ext.data.ArrayStore', {
+      fields: [ 'operator' ],
+      data: [['\='],['\!\='],['\>'],['\>\='],['\<'],['\<\='],['LIKE'],['NOT LIKE']]
+    }),//End create store
+    displayField: 'operator',
+    fieldLabel: 'Operator',
+    queryMode: 'local',
+    selectOnTab: false,
+    name: 'operator',
+  };
+
+  var comboValue = {
+    xtype: 'textfield',
+    fieldLabel: 'Value',
+    name: 'value',
+  };
+
+  window.fieldGroup = {
+    xtype: 'fieldcontainer',
+    defaults: {
+      allowBlank: false,
+      blankText: 'This is a required field',
+    },
+    items: [comboJoin, comboField, comboOperator, comboValue]
+  };
+
+  window.fieldGroup1 = {
+    xtype: 'fieldcontainer',
+    defaults: {
+      allowBlank: false,
+      blankText: 'This is a required field',
+    },
+    items: [comboField, comboOperator, comboValue]
+
+  };
+});
