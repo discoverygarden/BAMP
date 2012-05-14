824e9e8541a295a8711adad4de8d1fa83153dcb9
TJ Lewis <fuel37@hotmail.com>
2012-05-14 10:46:37 -0300

    updating csv functions and changing paths

--- mapping/dataHandler.php
+++ mapping/dataHandler.php
@@ -6,7 +6,7 @@
 require_once('dataHandler.class.php');
 
 $dbh = mysql_connect('localhost','root','q$%^az');
-switch($_GET['type']){
+switch($_REQUEST['type']){
   //Get marker data for when page is first loaded.
   //Seperated so we can remove tooltips etc if there are performance issues with showing ALL markers.
   case 'wild_site_markers':
@@ -85,30 +85,31 @@ switch($_GET['type']){
       $query = "SELECT column_name, column_comment,data_type, table_name ";
       $query.= "FROM information_schema.columns ";
       //The two tables that make up our view
-      $query.= "WHERE table_name IN ('bamp_wild_sampling_instances','bamp_wild_fish_samples') ";
+      $query.= "WHERE table_name IN ('bamp_wild_sampling_instances','bamp_wild_lice_details') ";
       //Fields to exclude as filters
-      $query.= "AND column_name NOT IN ('id','changelog','trip_id','year_rep_route_site','comments','site_number','fish','route','tissue_damage', 'latitude','longitude', 'date', 'waypoint')";
+      $query.= "AND column_name NOT IN ('id','changelog','trip_id','year_rep_route_site','comments','site_number','fish','route','tissue_damage', 'latitude','longitude', 'date', 'waypoint', 'trip_date','latitude_rec','latitude_calc','longitude_rec','longitude_calc', 'fish_id','lice_id','crew','search_time', 'salinity_0_2','salinity_1','salinity_5','salinity_refract','salinity_depth_not_specified','temperature_0_2','temperature_1','temperature_5','temperature_rec','temperature_depth_not_specified','weather_comments', 'distance','trip','trip_time','tsb','trip_set_id')";
       $result = mysql_query($query, $dbh) or die(mysql_error($dbh));
 
       while($row = mysql_fetch_assoc($result)){
         switch($row['table_name']){
           case 'bamp_wild_sampling_instances':
             $tableName = 'si';
-            $tableNameStr = 'Sampling_Instances';
+            $tableNameStr = 'Sampling Instances';
           break;
-          case 'bamp_wild_fish_samples':
-            $tableName = 'fs';
-            $tableNameStr = 'Fish_Samples';
+          case 'bamp_wild_lice_details':
+            $tableName = 'ld';
+            $tableNameStr = 'Lice Details';
           break;
         }//end switch
-        $columns[] = array('field'=>$row['column_comment'].' ['.$tableNameStr.'.'.$row['column_name'].']', 'fieldId'=>$tableName.'_'.$row['column_name']);
+        //$columns[] = array('field'=>$row['column_comment'].' ['.$tableNameStr.'.'.$row['column_name'].']', 'fieldId'=>$tableName.'_'.$row['column_name']);
+        $columns[] = array('field'=>$tableNameStr.': '.$row['column_comment'], 'fieldId'=>$tableName.'_'.$row['column_name']);
       }//end while
       echo json_encode($columns);
   break;
   //Process custom filters and return markers
   case 'customFilter':
     $dh = new dataHandler();
-    $data = $dh->getData(json_decode($_POST['customFilters']), json_decode($_POST['polygonPoints']));
+    $data = $dh->getData(json_decode($_REQUEST['customFilters']), json_decode($_REQUEST['polygonPoints']));
     echo json_encode($data);
   break;
   //Save selection data
