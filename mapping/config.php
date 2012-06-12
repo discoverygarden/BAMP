2cd39a08dba8c95efe917c1c1b83a45a3ad252dd
TJ Lewis <fuel37@hotmail.com>
2012-06-12 13:25:08 -0300

    Initial revision

---
+++ mapping/config.php
@@ -0,0 +1,12 @@
+<?php
+//Database
+$config['db']['host'] = '';
+$config['db']['name'] = '';
+$config['db']['user'] = '';
+$config['db']['pass'] = '';
+
+//Application paths
+$config['paths']['crudUrl'] = '?q=bampcrud/crud';//Url to crud forms
+$config['paths']['fishSamplesUrl'] = '?q=wild-fish-samples';//Wild Fish Samples View Report URL
+$config['paths']['markerImagesUrl'] = 'sites/default/modules/mapping/images/gmapicons';//Marker images URL
+?>
